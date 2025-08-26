72cc6db253f295f152f0ddd245e2ec1a09fecdec
Jin An <jin.an@dtcc.edu>
2025-08-26 10:15:04 -0400

    Create Main.java

---
+++ week01/tuesday/Main.java
@@ -0,0 +1,25 @@
+/*
+multi line comment
+in this class, you have to put in some stuff here
+
+Jin An
+CSC164
+8/26
+
+I did not cheat.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // like int main in cpp
+        System.out.printf("Hello and welcome!");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
+
+class Bob {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
